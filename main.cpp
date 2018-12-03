2b20be4982adf4125e461d802aa4ee36a86978b7
francozp <31630191+francozp@users.noreply.github.com>
2018-12-03 18:58:50 -0300

    Inicio files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    int l;
+    ifstream inFile;
+    inFile.open("Instancias/test1.txt");
     return 0;
 }
