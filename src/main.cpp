4c4eec32691112a2442709e90fe5f4c642cb5949
francozp <31630191+francozp@users.noreply.github.com>
2018-12-11 17:17:45 -0300

    hill climbing + restart

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,11 @@
 #include <time.h>
 using namespace std;
 
+//Parámetros
+#define RESTART 10;
+#define ITERATIONS 20;
+
 vector<int> gen_initial_solution(vector<int> carsQty,int num_classes){
-    srand(time(NULL));
     vector<int> initialsol;
     int cont = 0;
     int cont1 = 0;
@@ -48,16 +51,13 @@ int eval(vector<int> &solution,  vector<vector<int>> options, vector<int> blockS
     return eval;
 }
 
-void neighbor(vector<int> solution, int Position, int nextPosition){
-    iter_swap(solution.begin() + Position,solution.begin()+ nextPosition);
-}
-
 vector<int> climbing(int actual_eval, vector<int> &solution,  vector<vector<int>> options, vector<int> blockSize, vector<int> carsBlock, int num_vehicles, int num_options){
-    int min = actual_eval;
+    int min = actual_eval;// AL ESCALAR VERIFICAR PUNTOS EN QUE SE MUEVE ENTRE DOS SOLUCIONES, GUARDAR LA PENULTIMA PARA VERIFICIAR SI ES IGUAL A LA ACTUAL, EN CUYO CASO SE HACE BREAK
     int neighbor_eval;
     vector<int> bestClimb;
+    copy(solution.begin(), solution.end(), back_inserter(bestClimb)); 
     for(int i = 0; i < num_vehicles - 1; i++){
-        iter_swap(solution.begin() + i,solution.begin()+ i + 1);
+        iter_swap(solution.begin() + i,solution.begin()+ i + 1); //vecino
         neighbor_eval = eval(solution,options, blockSize, carsBlock, num_vehicles, num_options);
         if(neighbor_eval <= min){
             min = neighbor_eval;
@@ -65,18 +65,35 @@ vector<int> climbing(int actual_eval, vector<int> &solution,  vector<vector<int>
         }
         iter_swap(solution.begin() + i,solution.begin()+ i + 1);
     } 
-    int cont = 0;
-    while(cont < num_vehicles){
+    /*while(cont < num_vehicles){
         cout << bestClimb[cont];
         cont++;
-    }
-    cout << "\n";
+    }*/
+    //cout << "\n";
     return bestClimb;
+}
+
+pair<vector<int>, int> HillClimbing(int iteraciones, vector<vector<int>> options, vector<int> blockSize, vector<int> carsBlock, int num_vehicles, int num_options, vector<int> carsQty, int num_classes){
+    vector <int> initial_sol = gen_initial_solution(carsQty, num_classes);
+    int actual_eval = eval(initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
+    //cout << actual_eval << "\n";
+    for(int k=0; k < iteraciones; k++){
+        initial_sol = climbing(actual_eval,initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
+        actual_eval = eval(initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
+    }
+    //cout << eval(initial_sol,options, blockSize, carsBlock, num_vehicles, num_options) << "\n";
+    pair<vector<int>, int> sol = make_pair(initial_sol,actual_eval);
+    return sol;
 }
 
 int main()
 {   
-    vector<int> initial_sol;
+    srand(time(NULL));
+    vector<int> bestSolution;
+    int bestEval;
+    pair<vector<int>, int> hc;
+    int restart = RESTART;
+    int iteraciones = ITERATIONS;
     int cont = 0;
     int clase = 0;
     int num_vehicles, num_classes, num_options;
@@ -126,18 +143,24 @@ int main()
             clase++;
         }
     }
-    initial_sol = gen_initial_solution(carsQty, num_classes);
-    cont= 0;
-    /*while(cont < num_vehicles){
-        cout << initial_sol[cont];
-        cont++;
-    }*/
-    int actual_eval = eval(initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
-    cout << actual_eval << "\n";
-    for(int k=0; k < 10; k++){
-        initial_sol = climbing(actual_eval,initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
-        actual_eval = eval(initial_sol, options, blockSize, carsBlock, num_vehicles, num_options);
+    hc = HillClimbing(iteraciones, options, blockSize, carsBlock,num_vehicles, num_options, carsQty, num_classes);
+    bestSolution = hc.first;
+    bestEval = hc.second;
+    while(restart){
+        hc = HillClimbing(iteraciones, options, blockSize, carsBlock,num_vehicles, num_options, carsQty, num_classes);
+        if(hc.second < bestEval){
+            bestSolution = hc.first;
+            bestEval = hc.second;
+        }
+        restart --;
     }
-    cout << eval(initial_sol,options, blockSize, carsBlock, num_vehicles, num_options);
+    cont = 0;
+    cout << "La mejor evaluacion es: " <<bestEval<< "\n";
+    cout << "La secuencia es: ";
+    while(cont < num_vehicles){
+        cout << bestSolution[cont] << " ";
+        cont++;
+    }
+    cout << "\n";
     return 0;
 }
